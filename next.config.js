5b4e50c7836904d89a642d203679b215fd22c553
Frankimone <francistogor@gmail.com>
2025-12-06 15:21:28 +0100

    Remove duplicate next.config.js (use next.config.ts instead)

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  images: { unoptimized: true },
-};
-
-module.exports = nextConfig;
