1fe7a43ba16a0747843293e48fc8908ffb80bb87
Frankimone <francistogor@gmail.com>
2025-12-09 20:08:58 +0100

    modified:   .firebase/hosting.b3V0.cache 	deleted:    firebase-debug.log 	modified:   src/components/providers.tsx

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,14 +1,11 @@
 'use client'
 
-import { SessionProvider } from "next-auth/react"
 import { AppThemeProvider } from "./theme-provider"
 
 export function RootProviders({ children }: { children: React.ReactNode }) {
     return (
-        <SessionProvider>
-            <AppThemeProvider>
-                {children}
-            </AppThemeProvider>
-        </SessionProvider>
+        <AppThemeProvider>
+            {children}
+        </AppThemeProvider>
     )
 }
