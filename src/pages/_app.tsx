61c61b56547dfe905d22861faaecf7a69411fd0c
Frankimone <francistogor@gmail.com>
2025-12-27 21:23:43 +0100

    modified:   src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
 import '@/app/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { App as CapacitorApp } from '@capacitor/app'
+import { Capacitor } from '@capacitor/core'
 
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        // Handle Android back button
+        if (Capacitor.isNativePlatform()) {
+            const handleBackButton = CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+                if (canGoBack) {
+                    window.history.back()
+                } else {
+                    // At root, exit the app
+                    CapacitorApp.exitApp()
+                }
+            })
+
+            return () => {
+                handleBackButton.then(listener => listener.remove())
+            }
+        }
+    }, [])
+
     return (
         <>
             <Head>
